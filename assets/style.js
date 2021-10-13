c26316e2dc95ab76295c04304469951a06ae7c96
ayonsa60 <ayonsa60@gmail.com>
2021-10-12 20:18:16 -0500

    first bulma commit

---
+++ assets/style.js
@@ -0,0 +1,8 @@
+// mobile menu
+var burgerIcon = document.querySelector('#burger');
+var navbarMenu = document.querySelector('#nav-links');
+
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
