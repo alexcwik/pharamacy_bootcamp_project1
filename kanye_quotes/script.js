7e453b4072710f1689df4c7daa857595c68039a1
alexcwik <89872694+alexcwik@users.noreply.github.com>
2021-10-12 23:55:34 -0500

    Add files via upload
    kanye quotes with some added styles and a photo

--- kanye_quotes/script.js
+++ kanye_quotes/script.js
@@ -1,6 +1,21 @@
+var quoteResponseEl = document.getElementById("#response");
+var buttonSubmitEl = document.getElementById("#quoteGenerator")
+
 let quotesDiv = document.getElementById('quotes')
 fetch('https://api.kanye.rest')
    .then(res => res.json())
    .then(quote => {
       quotesDiv.innerHTML += `<p>${quote.quote}</p>`
    })
+
+function showResponse(event){
+   event.preventDefault();
+   console.log(event);
+   var quotes = quoteResponseEl.textContent = response;
+
+}
+
+buttonSubmitEl.addEventListener("click", quoteGenerator);
+
+
+
