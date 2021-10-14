65b74cfc356ece0a633540c355ca2f2d11ae8e06
Alex Cwik <alexcwik1490@gmail.com>
2021-10-14 10:08:46 -0500

    added youtube api

--- kanye/kanye_quotes/script.js
+++ kanye/kanye_quotes/script.js
@@ -1,11 +1,12 @@
 var quoteResponseEl = document.getElementById("#response");
 var buttonSubmitEl = document.getElementById("#quoteGenerator")
 
-let quotesDiv = document.getElementById('quotes')
-fetch('https://api.kanye.rest')
-   .then(res => res.json())
-   .then(quote => {
-      quotesDiv.innerHTML += `<p>${quote.quote}</p>`
+let quotesDiv = document.getElementById('quotes') // declares the quotes div as a variable
+
+fetch('https://api.kanye.rest') //returns kanye api as an object
+   .then(res => res.json())// parses response as a object
+   .then(quote => { //calls back new json
+      quotesDiv.innerHTML += `<p>${quote.quote}</p>`//allows to put json object to text in DOM
    })
 
 function showResponse(event){
@@ -15,5 +16,36 @@ function showResponse(event){
 
 }
 
-buttonSubmitEl.addEventListener("click", quoteGenerator);
-
+function getVideo() {
+   $.ajax({
+     type: 'GET',
+     url: 'https://www.googleapis.com/youtube/v3/playlistItems',
+     data: {
+         key: 'AIzaSyAIsi7xMBnzvqyINs3IFXe1atvBBb57MQ8',
+         part: 'snippet',
+         maxResults: 25,
+         playlistId: 'PLkd5lw_1df5DnJ_b1cwUwUMV9y5RVs0Oy',
+         
+     },
+     success: function(data){
+         embedVideo(data)
+         
+       
+     },
+     error: function(response){
+         console.log("Request Failed");
+     }
+   });
+ }
+ 
+ 
+ 
+ function embedVideo(data) {
+ $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[15].id.playlistId)
+ $('h3').text(data.items[15].snippet.title)
+ $('.description').text(data.items[15].snippet.description)
+ }
+ 
+ 
+ 
+ getVideo();
