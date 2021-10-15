76129db7bfb1d5a06c91d418b40f7e5a69527e3d
alexcwik <89872694+alexcwik@users.noreply.github.com>
2021-10-14 19:09:54 -0500

    Add files via upload

--- assets/style.js
+++ assets/style.js
@@ -3,6 +3,11 @@ var burgerIcon = document.querySelector('#burger');
 var navbarMenu = document.querySelector('#nav-links');
 var quoteResponseEl = document.getElementById("#response");
 var buttonSubmitEl = document.getElementById("#quoteGenerator")
+let kanyeButton = document.getElementById('kanyeQuotes')
+let previousQuote 
+
+var currentQuote = 0;
+let currentQuoteIndex = 0
 
 
 burgerIcon.addEventListener('click', () => {
@@ -16,23 +21,24 @@ burgerIcon.addEventListener('click', () => {
 //     });
 // });
 
-
-let quotesDiv = document.getElementById('quotes') // declares the quotes div as a variable
-
-fetch('https://api.kanye.rest') //returns kanye api as an object
-  .then(res => res.json())// parses response as a object
-   .then(quote => { //calls back new json
-      quotesDiv.innerHTML += `<p>${quote.quote}</p>`//allows to put json object to text in DOM
-  })
+//NOTE: Quote button works and is now the photo. The quote generator keeps generating quotes without hiding the previous one. Tried many things can't get it to work. Will keep trying.
+let quotesDiv = document.getElementById('kanyeQuotes') // declares the quotes div as a variable
+quotesDiv.addEventListener("click", evt =>{
+    fetch('https://api.kanye.rest') //returns kanye api as an object
+    .then(res => res.json())// parses response as a object
+    .then(quote => { //calls back new json
+    quotesDiv.innerHTML += `<p>${quote.quote}</p>`//allows to put json object to text in DOM
+    
+ });     
+}
+)
 
 function showResponse(event){
-  event.preventDefault();
-  console.log(event);
-  var quotes = quoteResponseEl.textContent = response;
-
+  
 }
 
 
+//NOTE: The youtube playlist is stored in an array but cannot get all the videos to display. Only one will show at a single time. And only if the number in the indexes in the embedVideo function are changed from 1-20. 
 function getVideo() { 
   $.ajax({
     type: 'GET', 
@@ -53,8 +59,6 @@ function getVideo() {
     }
   });
 }
-
-
 
 function embedVideo(data) { 
   console.log(data)
